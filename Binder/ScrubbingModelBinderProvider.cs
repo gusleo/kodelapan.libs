901d7591b7d05d65941070b4a2c3f75528b677d8
Gus Leo <gusleo.bali@gmail.com>
2016-08-25 16:37:50 +0800

    Initial commit repo

--- Binder/ScrubbingModelBinderProvider.cs
+++ Binder/ScrubbingModelBinderProvider.cs
@@ -15,13 +15,20 @@ namespace Kodelapan.Libs.Binder
 
             if ( !context.Metadata.IsComplexType )
             {
-                // Look for scrubber attributes
-                var propName = context.Metadata.PropertyName;
-                var propInfo = context.Metadata.ContainerType.GetProperty(propName);                
+                try
+                {
+                    // Look for scrubber attributes
+                    var propName = context.Metadata.PropertyName;
+                    var propInfo = context.Metadata.ContainerType.GetProperty(propName);
 
-                // Only one scrubber attribute can be applied to each property
-                var attribute = propInfo.GetCustomAttributes(typeof(IScrubberAttribute), false).FirstOrDefault();
-                if ( attribute != null ) return new ScrubbingModelBinder(context.Metadata.ModelType, attribute as IScrubberAttribute);
+                    // Only one scrubber attribute can be applied to each property
+                    var attribute = propInfo.GetCustomAttributes(typeof(IScrubberAttribute), false).FirstOrDefault();
+                    if ( attribute != null ) return new ScrubbingModelBinder(context.Metadata.ModelType, attribute as IScrubberAttribute);
+                }catch(Exception ex )
+                {
+                    return null;
+                }
+               
             }
 
             return null;
